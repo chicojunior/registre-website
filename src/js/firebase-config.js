3dc928f5a73681e1398d9357efb5bf525e4f8fac
francisco.vale <francisco.vale@ifactory.com.br>
2017-04-10 14:22:11 -0300

    Iniciando o boilerplate

---
+++ src/js/firebase-config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCAANKoGQI4K49wt9V2Z2mtLr189TgabN8",
+	authDomain: "registre-website.firebaseapp.com",
+	databaseURL: "https://registre-website.firebaseio.com",
+	projectId: "registre-website",
+	storageBucket: "registre-website.appspot.com",
+	messagingSenderId: "943539225073"
+};
+firebase.initializeApp(config);
